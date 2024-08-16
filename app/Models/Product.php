73bb6ba5fd8d83edf21ab1ceee522bf79e440627
TaliesinY <taliesinyip@gmail.com>
2024-08-16 18:15:50 -0400

    first commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,32 +5,10 @@ namespace App\Models;
 class Product
 {
     public static function all(){
-        return [
-            [
-                'id' => 1,
-                'name' => 'product1',
-                'cost' => 10,
-            ],
-            [
-                'id' => 2,
-                'name' => 'product2',
-                'cost' => 20,
-            ],
-            [
-                'id' => 3,
-                'name' => 'product3',
-                'cost' => 30,
-            ]
-        ];
+        return;
     }
 
     public static function find($id){
-        $products = self::all();
 
-        foreach ($products as $product) {
-            if ($product['id'] == $id) {
-                return $product;
-            }
-        }
     }
 }
