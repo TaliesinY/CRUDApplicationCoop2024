c13130302f548683952ea84647eae70232d6292f
TaliesinY <taliesinyip@gmail.com>
2024-08-21 13:59:56 -0400

    commit

---
+++ resources/views/products.blade.php
@@ -0,0 +1,10 @@
+<h1>{{$heading}}</h1>
+
+@unless(count($products) == 0)
+@foreach($products as $product)
+    <h2><a href="/products/{{$product['id']}}">{{$product['name']}}</a></h2>
+    <h2>{{$product['price']}}</h2>
+@endforeach
+@else
+<p>No listings Found</p>
+@endunless
